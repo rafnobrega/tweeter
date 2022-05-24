8be7882f6c2d86fe51da6ed8dacb12f586541c3f
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-23 23:47:14 -0700

    add counter functioning

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("textarea").on("input", function (event) {
+    const chars = $(this).val();
+    const counter = $(this).parent().find(".counter");
+    let remaining = 140 - chars.length;
+    counter.text(remaining);
+    if (remaining < 0) {
+      counter.addClass("counter-negative");
+    } else {
+      counter.removeClass("counter-negative");
+    }
+  });
+});
