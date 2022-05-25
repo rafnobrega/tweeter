7b22da1fe49323ffb564ad52da49f3ca7d372be6
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-25 14:08:17 -0700

    fetch tweets with Ajax and render them in the timeline

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,38 +5,16 @@
  */
 
 $(document).ready(function () {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@Descartegram",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1261113959088,
-    },
-  ];
+
 
   const renderTweets = function (tweets) {
     let container = $(".timeline");
+    container.empty();
     for (let tweet of tweets) {
       let $tweet = createTweetElement(tweet);
-      container.append($tweet); // might want to change to prepend
+      container.prepend($tweet); 
     }
-    // gotta empty the tweet container
+    
   };
 
   const createTweetElement = function (data) {
@@ -75,12 +53,20 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  renderTweets(data);
-
   $("form").submit(function (event) {
     event.preventDefault();
     let $tweet = $("#tweet-text").serialize();
     console.log("MY TWEET:", $tweet);
-    $.ajax("/tweets", { method: "POST", data: $tweet });
+    $.ajax("/tweets", { method: "POST", data: $tweet }).then(function () {
+      loadTweets();
+    })
   });
+
+  const loadTweets = function () {
+    $.get("/tweets").then(function (data) {
+      console.log("Success! loadTweets was called.");
+      return renderTweets(data);
+    });
+  }
+  loadTweets();
 });
