194dbcc7e7a65cd0c7204c91827c44091e0106a5
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-24 18:47:35 -0700

    create renderTweets function in client.js. Review and refactor a few lines with a mentor

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,57 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
 // Test / driver code (temporary). Eventually will get this from the server.
-$(document).ready(function() {
-const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
+$(document).ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
       },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
       },
-    "created_at": 1461116232227
- }
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@Descartegram",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1261113959088,
+    },
+  ];
 
-const createTweetElement = function (dataObj) {
-  let name = dataObj["user"]["name"];
-  let avatars = dataObj["user"]["avatars"];
-  let handle = dataObj["user"]["handle"];
-  let text = dataObj["content"]["text"];
-  let date = new Date(dataObj["created_at"]);
-  date = date.toDateString();
+  const renderTweets = function (tweets) {
+    let container = $(".timeline");
+    for (let tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      container.append($tweet); // might want to change to prepend
+    }
+    // gotta empty the tweet container
+  };
 
-  const $tweet = $(`
+  const createTweetElement = function (data) {
+    const {
+      user: {
+        name,
+        avatars,
+        handle
+      },
+      content: {
+        text
+      },
+    } = data;
+    let date = new Date(data.created_at);
+    date = date.toDateString();
+
+    let $tweet = $(`
   <article class="tweet">
   <header>
           <div class="user-info">
@@ -53,14 +79,10 @@ const createTweetElement = function (dataObj) {
   
   
   </article>`);
-  return $tweet;
-};
+    return $tweet;
+  };
 
+  renderTweets(data);
 
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.timeline').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  
 });
